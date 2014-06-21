7c17bc728cdae8aedae84aef032ff88efcc90be8
Roger Jin <yangzhen.jin@hp.com>
2014-06-21 23:10:41 +0800

    runOnUiThread

--- src/android/com/hp/dreadwing/plugin/UiWrapper.java
+++ src/android/com/hp/dreadwing/plugin/UiWrapper.java
@@ -3,10 +3,13 @@
  */
 package com.hp.dreadwing.plugin;
 
+import android.util.Log;
+import org.apache.cordova.CordovaActivity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+
 import com.hp.dreadwing.Mobility;
 
 /**
@@ -19,15 +22,23 @@ public class UiWrapper extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        final CallbackContext ctxt = callbackContext;
 
         if (action.equals(ACTION_RELOAD)) {
-            Mobility ma = (Mobility) cordova.getActivity();
-            ma.reloadMainView();
-            callbackContext.success();
+            final Mobility act = (Mobility)cordova.getActivity();
+            act.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    act.reloadMainView();
+                    Log.d("UiWrapper", "called by ui thread");
+                    ctxt.success("1234");
+                }
+            });
+
             return true;
         }
 
-        callbackContext.error("Invalid action");
+        callbackContext.error("Invalid action" + action + args);
         return false;
     }
 }
